57bddb3636d89bfffe93473494c26321c977e0dc
poyo <poyo@poyopc.poyonet>
2024-11-29 18:54:30 +0100

    first image compiled!

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # oreneta
 OS written in rust by hack clubbers
+`rustup toolchain install nightly` to install nightly rust.
+`cargo rustc -- -C link-arg=-nostartfiles` to compile.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+// Disable linking to the rust standard library
+// This is needed because the standard library relies on system functions.
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+// done
+// thank you
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
+
+// This has to be set to no_mangle because it will be called from assembly.
+// Rust by default mangles function and varaible names to include stuff like
+// type information. We cannot have that.
+// _start is UNIX/ELF callZing convention
+// Okay, I asked them to stop having conversations in my thread :)
+
+// lmao sure
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+// feel free to leave silly easter eggs in the comments
+// This should work shouldnt it?
