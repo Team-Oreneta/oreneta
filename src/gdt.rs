1b44023f8abb77da87403f682acae98fd298cdef
AI314159 <104926797+AI314159@users.noreply.github.com>
2024-12-05 11:12:40 -0500

    Started doing GDT changes.

--- src/gdt.rs
+++ src/gdt.rs
@@ -1,3 +1,5 @@
+use core::{mem, ptr};
+
 #[repr(C, packed(2))]
 struct GdtEntry {
     limit_low: u16,
@@ -8,19 +10,41 @@ struct GdtEntry {
     base_high: u8,
 }
 
-#[repr(C, packed(2))]
+#[repr(C, packed)]
 struct GdtPtr {
     limit: u16,
     base: u32,
 }
-static mut GDT: [GdtEntry; 4] = [
+static mut GDT: [GdtEntry; 3] = [
     GdtEntry::new(0, 0, 0, 0),
-    GdtEntry::new(0, 0xFFFFF),
-    
+    GdtEntry::new(0, 0xFFFFFFFF, 0x9A, 0xCF),
+    GdtEntry::new(0, 0xFFFFFFFF, 0x92, 0xCF),
 ];
-static mut gdt: [GdtPtr; 4] = [NULL, NULL, NULL, NULL];
-static mut gp: GdtPtr = GdtPtr {
-    limit: 0,
-    base: 0,
-};
-// YES: https://github.com/redox-os/kernel/blob/master/src/arch/x86/gdt.rs
+
+#[no_mangle]
+pub static mut GP: GdtPtr = GdtPtr { base: 0, limit: 0 };
+
+extern "C" {
+    fn gdt_flush();
+}
+
+pub fn init_gdt() {
+    unsafe {
+        GP.base = GDT.as_ptr() as u32;
+        GP.limit = (GDT.len() * mem::size_of::<GdtEntry>() - 1) as u16;
+        gdt_flush();
+    }
+}
+
+impl GdtEntry {
+    pub const fn new(base: u32, limit: u32, access: u8, gran: u8) -> Self {
+        GdtEntry {
+            limit_low: (limit & 0xFFFF) as u16,
+            base_low: (base & 0xFFFF) as u16,
+            base_middle: ((base >> 16) & 0xFF) as u8,
+            access: access,
+            granularity: (((limit >> 16) & 0x0F) as u8) | (gran & 0xF0),
+            base_high: ((base >> 24) & 0xFF) as u8,
+        }
+    }
+}
