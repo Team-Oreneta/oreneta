dc9bf7fbb2114154d13fc4129b49f0a8cdcd7a08
AI314159 <104926797+AI314159@users.noreply.github.com>
2024-12-05 19:17:24 -0500

    Create idt.rs

---
+++ src/idt.rs
@@ -0,0 +1,63 @@
+#[repr(C, packed(2))]
+#[derive(Copy, Clone)]
+struct IdtEntry {
+    base_low: u16,
+    sel: u16,
+    zero: u8,
+    flags: u8,
+    base_high: u16,
+}
+
+#[repr(C, packed(2))]
+struct IdtPtr {
+    limit: u16,
+    base: u32,
+}
+
+const INIT_ENTRY: IdtEntry = IdtEntry {
+    base_low: 0,
+    sel: 0,
+    zero: 0,
+    flags: 0,
+    base_high: 0,
+};
+
+static mut IDT: [IdtEntry; 256] = [ INIT_ENTRY; 256 ];
+
+#[no_mangle]
+static mut IDT_PTR: IdtPtr = IdtPtr {
+    limit: 0,
+    base: 0,
+};
+
+fn init_idt_ptr() {
+    unsafe {
+        IDT_PTR = IdtPtr {
+            limit: (core::mem::size_of::<[IdtEntry; 256]>() - 1) as u16,
+            base: IDT.as_ptr() as u32,
+        };
+    }
+}
+
+extern "C" {
+    fn load_idt();
+}
+
+fn idt_set_gate(num: u8, base: u32, sel: u16, flags: u8) {
+    unsafe {
+        IDT[num as usize] = IdtEntry {
+            base_low: (base & 0xFFFF) as u16,
+            sel: sel,
+            zero: 0,
+            flags: flags,
+            base_high: ((base >> 16) & 0xFFFF) as u16,
+        }
+    }
+}
+
+pub fn init_idt() {
+    unsafe {
+        init_idt_ptr();
+        load_idt();
+    }
+}
